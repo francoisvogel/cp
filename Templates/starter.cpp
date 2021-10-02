7c30b95f73522df952c1808f821275e0b8619b8d
Francois Vogel <francois.g.vogel@gmail.com>
2021-10-02 17:12:12 +0200

    added template

---
+++ Templates/starter.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pii pair<int, int>
+#define f first
+#define s second
+
+#define vi vector<int>
+#define all(x) x.begin(), x.end()
+#define size(x) (int)(x.size())
+#define pb push_back
+#define ins insert
+#define cls clear
+
+#define ll long long
+#define ld long double
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+signed main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    // code here
+
+    cout.flush();
+    int d = 0;
+    d++;
+}
